f4f5144b9cb9dcfdbc0e65317a35a040cc05c910
Victor Sriqui <victorsriqui@gmail.com>
2023-02-13 12:51:09 -0700

    Add for correction

--- for.js
+++ for.js
@@ -16,17 +16,16 @@ for ( n=25 ; n<=75 ; n++ ) {
 var weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"];
 for (var i = 0; i < weather_options.length; i++) {
   console.log("We are lookin at a weather forecast of " + weather_options[i])
-}
+}rm
 
 // Challenge 3
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
 
-var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
+var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"];
 for (var i=0; i<words.length; i++) {
-if ((words[i].length % 3) === 0) [
-  console.log(words[i])
-]
-  
+  if (words[i].length === 3)[
+    console.log(words[i])
+  ]
 }
 // 📹 Record yourself live-coding and talking through any ONE of the challenges. 
   // The recording doesn't have to be the first time you are solving the problem. 
